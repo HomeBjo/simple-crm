a3ece0e4f4ffbab9decf8ae0a827af8482e03e92
HomeBjo <bjoernteneicken@gmail.com>
2024-03-20 13:37:36 +0100

    add progressbar

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,21 +1,23 @@
-
-import { Component,inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {
   MatNativeDateModule,
   provideNativeDateAdapter,
 } from '@angular/material/core';
 import { User } from '../models/user.class';
-import { Firestore, collection, collectionData, addDoc  } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  addDoc,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
-
-
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -28,55 +30,36 @@ import { Observable } from 'rxjs';
     MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatProgressBarModule,
   ],
-  providers:[provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
-  
-   user = new User();
-   birthDate!: Date;
-   
-    firestore: Firestore = inject(Firestore);
-    items$: Observable<any[]>;
-  
-    constructor() {
-      const aCollection = collection(this.firestore, 'items')
-      this.items$ = collectionData(aCollection);
-    }
+  user = new User();
+  birthDate!: Date;
+  loading = false;
 
-    async saveUser(){
-      this.user.birthDate = this.birthDate.getTime();
-      console.log('CURRENT user is',this.user)
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
 
-       await addDoc(this.getUserRef(),this.user.toJSON())
-        console.log('adding user')
-      };
+  constructor() {
+    const aCollection = collection(this.firestore, 'items');
+    this.items$ = collectionData(aCollection);
+  }
 
+  async saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('CURRENT user is', this.user);
+    this.loading = true;
 
-   
+    await addDoc(this.getUserRef(), this.user.toJSON());
+    console.log('adding user');
+    this.loading = false;
+  }
 
-    getUserRef(){
-      return collection(this.firestore,'users')
-    }
-
-     
-    // addNote(item:{}){
-    //   return addDoc(this.getUserRef(),item)
-    //   }
-   
+  getUserRef() {
+    return collection(this.firestore, 'users');
+  }
 }
-
-
-
-
-
-
-// saveUser(){
-//   this.user.birthDate = this.birthDate.getTime();
-//   console.log('CURRENT user is',this.user)
-//   this.firestore.collection('users').add(this.user.toJSON()).then((result:any)=>{
-//     console.log('adding user',result)
-//   });
-// }
