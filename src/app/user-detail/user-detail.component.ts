3fc882c6ec3545ee13b76e6f2c882f0baba69bcc
HomeBjo <bjoernteneicken@gmail.com>
2024-03-25 15:37:02 +0100

    add COPY JSON

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -65,10 +65,10 @@ getUser() {
 }
 userEditMenu(){
   const dialog = this.dialog.open(DialogEditUserComponent);
-  dialog.componentInstance.user = this.user;
+  dialog.componentInstance.user = new User(this.user);
 }
 editMenu(){
   const dialog = this.dialog.open(DialogEditAdressComponent);
-  dialog.componentInstance.user = this.user;
+  dialog.componentInstance.user = new User(this.user);
 }
 }
